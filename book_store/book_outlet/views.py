7d7e017d87b8832cc9126fd55582ad13b4b2192d
patelmukundn <patelmukundn@gmail.com>
2021-06-26 18:06:47 +0530

    view with db and reverse url method model

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
+from .models import Book
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'book_outlet/index.html')
+    books_list = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {
+                  'books_list': books_list
+                  })
+
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk=id)
+    return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestselling': book.is_bestselling
+    })
