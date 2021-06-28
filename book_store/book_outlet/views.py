b25b883fd4f1a4012d625ea95be97a40fc86aa78
patelmukundn <patelmukundn@gmail.com>
2021-06-28 17:44:45 +0530

    Db complate

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 # Create your views here.
 
 
 def index(request):
-    books_list = Book.objects.all()
+    books_list = Book.objects.all().order_by("-rating")
+    avg_rating = books_list.aggregate(Avg("rating"))
+    total_books = books_list.count()
+
     return render(request, 'book_outlet/index.html', {
-                  'books_list': books_list
+                  'books_list': books_list,
+                  'avg_rating': avg_rating,
+                  'total_books': total_books
                   })
 
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     raise Http404()
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'book_outlet/book_detail.html', {
         'title': book.title,
         'author': book.author,
