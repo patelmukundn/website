93f2dc46122ce70e66a194b1389815fb7d296357
patelmukundn <patelmukundn@gmail.com>
2021-06-29 17:26:59 +0530

    ration db related_name

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models.deletion import CASCADE
 from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(
+        Author, on_delete=CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args,**kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
